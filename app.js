f5e7beb45b1ae79ff81b3d8b7af9e60fbe49a10d
Nate <gzyunke@qq.com>
2021-12-08 20:27:01 +0800

    调整代码

--- app.js
+++ app.js
@@ -11,18 +11,6 @@ log4js.configure('./log4js.json');
 log4js.level = 'DEBUG';
 let logger = log4js.getLogger('app');
 
-let isDocker = false;
-try{
-	const fs = require('fs')
-	// 判断是否在 Docker 里面运行
-	fs.accessSync('/.dockerenv');
-	isDocker = true;
-	logger.info('run in docker')
-}
-catch (e) {
-	isDocker = false
-	logger.info('not in docker')
-}
 
 // 获取服务器 ip、hostname
 function getServerInfo() {
@@ -47,62 +35,73 @@ router.all('/hello/:name', async ctx =>{
 	ctx.body = `hello ${name ? name : 'world'} \n\n${getServerInfo()}\n`
 });
 
-
-let url = `mongodb://localhost:27018`;
-if(isDocker)
-{
-	url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ADDRESS}`
-}
-
-const MongoDB = require('mongodb')
-const mongoClient = new MongoDB.MongoClient(url);
-mongoClient.connect((err) =>{
-	if(!err)
-	{
-		logger.info(`MongoDB Connected successfully`);
-		app.context.db = mongoClient.db("testdb");
-	}
-	else {
-		logger.error('connect mongo failed:%s', err)
-	}
-});
-
-
-router.all('/regist', async ctx =>{
-	let username = ctx.request.body.username
-	let password = ctx.request.body.password
-	if(!username || !password){
-		ctx.body = {code: -1, msg: '参数不全'}
-		return
-	}
-	try{
-		await ctx.db.collection("users").updateOne({_id: username}, {'$set': {password: password}}, {upsert: true})
-		ctx.body = {code: 0, msg: 'success'}
-	}
-	catch (e) {
-		ctx.body = {code: -10, msg: `注册失败 ${e}`}
-	}
-});
-
-router.all('/login', async ctx =>{
-	let username = ctx.request.body.username
-	let password = ctx.request.body.password
-	if(!username || !password){
-		ctx.body = {code: -1, msg: '参数不全'}
-		return
-	}
-	try{
-		let user = await ctx.db.collection("users").findOne({_id: username, password})
-		if(user)
-			ctx.body = user
-		else
-			ctx.body = {code: -10, msg: '登录失败'}
-	}
-	catch (e) {
-		ctx.body = {code: -20, msg: `登录失败 ${e}`}
-	}
-});
-
+// let isDocker = false;
+// try{
+// 	const fs = require('fs')
+// 	// 判断是否在 Docker 里面运行
+// 	fs.accessSync('/.dockerenv');
+// 	isDocker = true;
+// 	logger.info('run in docker')
+// }
+// catch (e) {
+// 	isDocker = false
+// 	logger.info('not in docker')
+// }
+//
+// let url = `mongodb://localhost:27018`;
+// if(isDocker)
+// {
+// 	url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ADDRESS}`
+// }
+//
+// const MongoDB = require('mongodb')
+// const mongoClient = new MongoDB.MongoClient(url);
+// mongoClient.connect((err) =>{
+// 	if(!err)
+// 	{
+// 		logger.info(`MongoDB Connected successfully`);
+// 		app.context.db = mongoClient.db("testdb");
+// 	}
+// 	else {
+// 		logger.error('connect mongo failed:%s', err)
+// 	}
+// });
+//
+//
+// router.all('/regist', async ctx =>{
+// 	let username = ctx.request.body.username
+// 	let password = ctx.request.body.password
+// 	if(!username || !password){
+// 		ctx.body = {code: -1, msg: '参数不全'}
+// 		return
+// 	}
+// 	try{
+// 		await ctx.db.collection("users").updateOne({_id: username}, {'$set': {password: password}}, {upsert: true})
+// 		ctx.body = {code: 0, msg: 'success'}
+// 	}
+// 	catch (e) {
+// 		ctx.body = {code: -10, msg: `注册失败 ${e}`}
+// 	}
+// });
+//
+// router.all('/login', async ctx =>{
+// 	let username = ctx.request.body.username
+// 	let password = ctx.request.body.password
+// 	if(!username || !password){
+// 		ctx.body = {code: -1, msg: '参数不全'}
+// 		return
+// 	}
+// 	try{
+// 		let user = await ctx.db.collection("users").findOne({_id: username, password})
+// 		if(user)
+// 			ctx.body = user
+// 		else
+// 			ctx.body = {code: -10, msg: '登录失败'}
+// 	}
+// 	catch (e) {
+// 		ctx.body = {code: -20, msg: `登录失败 ${e}`}
+// 	}
+// });
 
 app.use(router.routes());
 
